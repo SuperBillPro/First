8e6ca364688fe9389f7b1058736ac531f014279c
seboya <juan.darosa78@gmail.com>
2026-03-18 08:22:47 -0300

    Agregada Base de Datos

--- login-registro/Controlador/servidor.php
+++ login-registro/Controlador/servidor.php
@@ -1,4 +1,25 @@
 <?php
+// Database configuration
+	$hostname = 'localhost';
+	$username = 'root';
+	$password = '';
+	$database = 'base_usuarios';
+	// Establish database connection
+	$conn = mysqli_connect($hostname, $username, $password, $database);
+	// Check connection
+	if(!$conn){
+		die('Connection failed: ' . mysqli_connect_error());
+	}
+
+	// Make query
+	$query = "SELECT * FROM usuario";
+	$result = mysqli_query($conn, $query);
+	$usuarios = [];
+	while($row = mysqli_fetch_assoc($result)) {
+		$usuarios[] = $row;
+	}
+	print_r($usuarios);
+
 $user = $_POST["user"];
 $pass = $_POST["pass"];
 $correo = $_POST["correo"];
